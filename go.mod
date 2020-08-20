d01418a5d2818cca2d72bdb15ae5f04e8b9f045d
Peter Parada <parad.peter@gmail.com>
2020-08-20 23:05:59 +0200

    Load env file

---
+++ go.mod
@@ -0,0 +1,5 @@
+module peterparada.com/coin-watcher
+
+go 1.14
+
+require github.com/joho/godotenv v1.3.0 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	log.Println("Hello and TODO")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	log.Println("Starting server")
 }
