95c04cb5d55964a5cb4610bccf1212635d19afb9
Peter Parada <parad.peter@gmail.com>
2020-08-22 22:36:01 +0200

    Prepare server listeining

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module peterparada.com/coin-watcher
+module peterparada.com/online-bookmarks
 
 go 1.14
 
 require (
-	github.com/go-chi/chi v4.1.2+incompatible // indirect
+	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/joho/godotenv v1.3.0
 )
--- main.go
+++ main.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
+	"peterparada.com/online-bookmarks/pkg/common/cmd"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	router := cmd.CreateRouter()
+
+
+	var port string = os.Getenv("PORT") // || ":2999"
+
+	server := &http.Server{Addr: port, Handler: router}
+
+	go func () {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+	log.Printf("Online bookmarks api server is listening on %s", server.Addr)
+
+	if err := server.Close(); err != nil {
+		panic(err)
 	}
 
 	log.Println("Starting server")
