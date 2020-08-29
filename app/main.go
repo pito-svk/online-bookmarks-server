053546076a36c26fbbfffa882dfe62e474be5486
Peter Parada <parad.peter@gmail.com>
2020-08-29 23:47:38 +0200

    Update golang infrastructure

---
+++ app/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func loadConfig() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	loadConfig()
+
+	var port string = os.Getenv("ADDRESS")
+
+	if port == "" {
+		port = ":2999"
+	}
+}
