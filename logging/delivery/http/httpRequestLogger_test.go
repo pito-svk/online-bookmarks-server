87f35f618dc378aedd1aef5918764870fd81fe70
Peter Parada <parad.peter@gmail.com>
2020-09-10 00:40:56 +0200

    Fix IP case

--- logging/delivery/http/httpRequestLogger_test.go
+++ logging/delivery/http/httpRequestLogger_test.go
@@ -12,73 +12,73 @@ import (
 
 func TestIsPrivateIpAddress(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
-		isPrivateIp, err := isPrivateIPAddress("127.0.0.1")
+		isPrivateIP, err := isPrivateIPAddress("127.0.0.1")
 
 		assert.NoError(t, err)
-		assert.Equal(t, true, isPrivateIp)
+		assert.Equal(t, true, isPrivateIP)
 	})
 
 	t.Run("success 2", func(t *testing.T) {
-		isPrivateIp, err := isPrivateIPAddress("192.168.0.0")
+		isPrivateIP, err := isPrivateIPAddress("192.168.0.0")
 
 		assert.NoError(t, err)
-		assert.Equal(t, true, isPrivateIp)
+		assert.Equal(t, true, isPrivateIP)
 	})
 
 	t.Run("success 3", func(t *testing.T) {
-		isPrivateIp, err := isPrivateIPAddress("192.168.255.255")
+		isPrivateIP, err := isPrivateIPAddress("192.168.255.255")
 
 		assert.NoError(t, err)
-		assert.Equal(t, true, isPrivateIp)
+		assert.Equal(t, true, isPrivateIP)
 	})
 
 	t.Run("success 4", func(t *testing.T) {
-		isPrivateIp, err := isPrivateIPAddress("172.16.0.0")
+		isPrivateIP, err := isPrivateIPAddress("172.16.0.0")
 
 		assert.NoError(t, err)
-		assert.Equal(t, true, isPrivateIp)
+		assert.Equal(t, true, isPrivateIP)
 	})
 
 	t.Run("success 5", func(t *testing.T) {
-		isPrivateIp, err := isPrivateIPAddress("172.31.255.255")
+		isPrivateIP, err := isPrivateIPAddress("172.31.255.255")
 
 		assert.NoError(t, err)
-		assert.Equal(t, true, isPrivateIp)
+		assert.Equal(t, true, isPrivateIP)
 	})
 
 	t.Run("success 6", func(t *testing.T) {
-		isPrivateIp, err := isPrivateIPAddress("10.0.0.0")
+		isPrivateIP, err := isPrivateIPAddress("10.0.0.0")
 
 		assert.NoError(t, err)
-		assert.Equal(t, true, isPrivateIp)
+		assert.Equal(t, true, isPrivateIP)
 	})
 
 	t.Run("success 7", func(t *testing.T) {
-		isPrivateIp, err := isPrivateIPAddress("10.255.255.255")
+		isPrivateIP, err := isPrivateIPAddress("10.255.255.255")
 
 		assert.NoError(t, err)
-		assert.Equal(t, true, isPrivateIp)
+		assert.Equal(t, true, isPrivateIP)
 	})
 
 	t.Run("success 9 (public ip address)", func(t *testing.T) {
-		isPrivateIp, err := isPrivateIPAddress("217.73.23.164")
+		isPrivateIP, err := isPrivateIPAddress("217.73.23.164")
 
 		assert.NoError(t, err)
-		assert.Equal(t, false, isPrivateIp)
+		assert.Equal(t, false, isPrivateIP)
 	})
 
 	t.Run("success 10", func(t *testing.T) {
-		isPrivateIp, err := isPrivateIPAddress("127.0.0.0")
+		isPrivateIP, err := isPrivateIPAddress("127.0.0.0")
 
 		assert.NoError(t, err)
-		assert.Equal(t, true, isPrivateIp)
+		assert.Equal(t, true, isPrivateIP)
 	})
 
 	t.Run("success 11", func(t *testing.T) {
-		isPrivateIp, err := isPrivateIPAddress("169.254.0.0")
+		isPrivateIP, err := isPrivateIPAddress("169.254.0.0")
 
 		assert.NoError(t, err)
-		assert.Equal(t, true, isPrivateIp)
+		assert.Equal(t, true, isPrivateIP)
 	})
 }
 
