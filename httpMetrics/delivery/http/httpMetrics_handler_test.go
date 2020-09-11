f15f172285aa0f60523646e42bbb3ef55e4b8c5d
Peter Parada <parad.peter@gmail.com>
2020-09-11 20:33:52 +0200

    Prepare httpMetrics handler test module

--- httpMetrics/delivery/http/httpMetrics_handler_test.go
+++ httpMetrics/delivery/http/httpMetrics_handler_test.go
@@ -1,16 +1,30 @@
 package http
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"peterparada.com/online-bookmarks/domain/mocks"
 )
 
-func TestLogHTTPRequests(t *testing.T) {
+func TestLogHTTPMetrics(t *testing.T) {
 	mockUsecase := mocks.NewHTTPMetricsUsecase()
+	mockLogger := mocks.NewLogger()
 
 	t.Run("success", func(t *testing.T) {
 		w := httptest.NewRecorder()
+		r := httptest.NewRequest("GET", "/ping", strings.NewReader(""))
 
+		handler := HTTPMetricsHandler{
+			HTTPMetricsUsecase: mockUsecase,
+			Logger:             mockLogger,
+		}
+
+		genericHTTPHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+		httpMetricsHandler := handler.LogHTTPMetrics(genericHTTPHandler)
+
+		httpMetricsHandler.ServeHTTP(w, r)
 	})
 }
