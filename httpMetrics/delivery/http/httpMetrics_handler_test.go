cff93119ae9d302a4bdddd792a2fc5b823f7635f
Peter Parada <parad.peter@gmail.com>
2020-09-11 23:31:47 +0200

    Add TODOs

--- httpMetrics/delivery/http/httpMetrics_handler_test.go
+++ httpMetrics/delivery/http/httpMetrics_handler_test.go
@@ -36,6 +36,7 @@ func TestLogHTTPMetrics(t *testing.T) {
 		assert.Equal(t, "POST", requestMetrics.Method)
 		assert.Equal(t, "https://www.example.com", requestMetrics.Referer)
 		assert.Equal(t, "Mozilla/5.0 (Linux; Android 6.0.1; SM-G935S Build/MMB29K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Mobile Safari/537.36", requestMetrics.UserAgent)
+		// TODO: Test ip address and response metrics
 	})
 
 	t.Run("success 2", func(t *testing.T) {
@@ -60,5 +61,6 @@ func TestLogHTTPMetrics(t *testing.T) {
 		assert.Equal(t, "POST", requestMetrics.Method)
 		assert.Equal(t, "https://www.example.com/example", requestMetrics.Referer)
 		assert.Equal(t, "Mozilla/5.0 (Linux; Android 7.1.1; G8231 Build/41.2.A.0.219; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/59.0.3071.125 Mobile Safari/537.36", requestMetrics.UserAgent)
+		// TODO: Test ip address and response metrics
 	})
 }
