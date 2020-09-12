a27fd9ccaf90032da74bbeadef7bbfdc23a5f990
Peter Parada <parad.peter@gmail.com>
2020-09-12 16:34:32 +0200

    Add test case for deliverUserCreatedResponse function

--- auth/delivery/http/auth_handler_test.go
+++ auth/delivery/http/auth_handler_test.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -44,6 +45,33 @@ func TestValidateCreateUserInput(t *testing.T) {
 		err := validateCreateUserInput(&userData)
 
 		assert.Error(t, err)
+	})
+}
+
+func TestDeliverUserCreatedResponse(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		w := httptest.NewRecorder()
+
+		user := userCreatedResponse{
+			ID:        "5f555a4686dbe11fc3adbb9b",
+			Email:     "random@example.com",
+			FirstName: "John",
+			LastName:  "Doe",
+			AuthData: authData{
+				Token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmNWIyOTA5NjMwZTdmMmQ5NWU5MjZkMCJ9.CxxXHpVzS5f0Psl34iLXR9sg3HCEB0dYglMfhvWHoZ4",
+			},
+		}
+
+		deliverUserCreatedResponse(w, user)
+
+		assert.Equal(t, http.StatusCreated, w.Code)
+
+		var httpResponse userCreatedResponse
+		if err := json.NewDecoder(w.Result().Body).Decode(&httpResponse); err != nil {
+			log.Fatal(err)
+		}
+
+		assert.Equal(t, user, httpResponse)
 	})
 }
 
