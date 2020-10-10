f0c31a708a22786370bbd0b980b7daeeb8f1d9fe
Peter Parada <parad.peter@gmail.com>
2020-10-10 21:54:09 +0200

    git

--- user/entity/user.go
+++ user/entity/user.go
@@ -1,10 +1,5 @@
 package entity
 
-// import (
-// 	"go.mongodb.org/mongo-driver/bson/primitive"
-// 	"golang.org/x/crypto/bcrypt"
-// )
-
 type UserInterface interface {
 	SetID()
 	SetHashedPassword()
@@ -18,39 +13,3 @@ type User struct {
 	FirstName string
 	LastName  string
 }
-
-// func generateHexID() string {
-// 	return primitive.NewObjectID().Hex()
-// }
-
-// func generateID() string {
-// 	return generateHexID()
-// }
-
-// func hashPassword(password string) (string, error) {
-// 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-// 	if err != nil {
-// 		return "", err
-// 	}
-
-// 	return string(hashedPassword), nil
-// }
-
-// func (user *User) SetID() {
-// 	user.ID = generateID()
-// }
-
-// func (user *User) SetHashedPassword() error {
-// 	hashedPassword, err := hashPassword(user.Password)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	user.Password = hashedPassword
-
-// 	return nil
-// }
-
-// func (user *User) ClearPassword() {
-// 	user.Password = ""
-// }
